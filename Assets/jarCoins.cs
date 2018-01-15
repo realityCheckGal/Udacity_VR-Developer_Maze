bef37e31d410f06f589949068553f311ce7adb67
realityCheckGal <realityCheckGal@users.noreply.github.com>
2018-01-15 02:04:54 -0800

    Initial Submission, Request for Feedback
    Project is complete based in requirements.  I would like to add a counter for the coins and provide the score at the end, with the time to get in the temple to challenge the user to try again to collect more coins, faster.

---
+++ Assets/jarCoins.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jarCoins : MonoBehaviour {
+
+	public GameObject jarOfCoins;
+	public Coin[] coinList;
+
+    public void OnJarClicked() {
+
+    	for (int i=0; i<coinList.Length; i++) {
+    		Coin coin = coinList[i];
+	     	Instantiate(coin.coinPoof, coin.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+	    	Destroy(coin.gameObject); 
+	    	// All five happen almost instantaneously
+	    	// How can I pause so the player hears 5 distinct coins?  
+	    	// WaitForSeconds(2);		
+    	}
+    	Debug.Log("Awesome!  Just just collected 5 coins!");
+       
+    }
+}
